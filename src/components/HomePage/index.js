69219cef9556a01e54e19fd748a7f2f98ba7148e
SparkCode <iseb@outlook.com>
2017-08-21 12:59:49 +0400

    initial commit

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+HomePage.propTypes = {};
+HomePage.defaultProps = {};
+
+export default HomePage;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './stylesheets/main.css'
+
+ReactDOM.render(<div/>, document.getElementById('root'));
