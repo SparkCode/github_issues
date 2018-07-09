dbcb76e00cea1f7f2fe64a8d471e7fb1618e87cb
SparkCode <sebeldin@mcb.ru>
2018-07-09 11:56:55 +0300

    fix a bug related to incorrect import

--- app/utils/network/index.js
+++ app/utils/network/index.js
@@ -1,4 +1,4 @@
 export NetworkError from './NetworkError';
-export UnsuccessfulRequestError from './NetworkError';
+export UnsuccessfulRequestError from './UnsuccessfulRequestError';
 export makeRequest from './makeRequest';
 export { default } from './makeRequest';
