45f5a6f3885aee969a189fee470bb8da34b258ac
SparkCode <iseb@outlook.com>
2018-08-09 02:46:06 +0300

    let's try to dynamically preconnect to http://api.github.com

--- app/containers/GithubIssuesPage/GithubIssuesPage.js
+++ app/containers/GithubIssuesPage/GithubIssuesPage.js
@@ -5,16 +5,27 @@ import IssuesList from 'containers/IssuesList/Loadable';
 import IssueDetail from 'containers/IssueDetail/Loadable';
 import DefaultValuesContext from './DefaultValuesContext';
 
-export default () => (
-  <DefaultValuesContext.Provider
-    value={{
-      issuesCountOnPageOptions: ['10', '20', '30', '50', '100'],
-      defaultIssuesCountOnPageOption: '20',
-    }}
-  >
-    <Route key="empty-search" exact path="/github-issues" component={IssuesSearch} />
-    <Route key="search" path="/github-issues/:userName/:repoName" component={IssuesSearch} />
-    <Route key="issues" exact path="/github-issues/:userName/:repoName" component={IssuesList} />
-    <Route key="issue" exact path="/github-issues/:userName/:repoName/:issueNumber" component={IssueDetail} />
-  </DefaultValuesContext.Provider>
-);
+const preconnectToGithub = () => {
+  const head = document.getElementsByTagName('head')[0];
+  const preconnectLink = document.createElement('link');
+  preconnectLink.rel = 'preconnect';
+  preconnectLink.href = 'https://api.github.com';
+  head.appendChild(preconnectLink);
+};
+
+export default () => {
+  preconnectToGithub();
+  return (
+    <DefaultValuesContext.Provider
+      value={{
+        issuesCountOnPageOptions: ['10', '20', '30', '50', '100'],
+        defaultIssuesCountOnPageOption: '20',
+      }}
+    >
+      <Route key="empty-search" exact path="/github-issues" component={IssuesSearch} />
+      <Route key="search" path="/github-issues/:userName/:repoName" component={IssuesSearch} />
+      <Route key="issues" exact path="/github-issues/:userName/:repoName" component={IssuesList} />
+      <Route key="issue" exact path="/github-issues/:userName/:repoName/:issueNumber" component={IssueDetail} />
+    </DefaultValuesContext.Provider>
+  );
+};
