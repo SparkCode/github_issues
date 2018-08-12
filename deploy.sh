c8dc347dd78de5bdef9e9d668e10fa478bf6250d
SparkCode <iseb@outlook.com>
2018-08-13 00:41:43 +0300

    change deployment logic

--- deploy.sh
+++ deploy.sh
@@ -77,7 +77,7 @@ selectNodeVersion () {
       NODE_EXE=`cat "$DEPLOYMENT_TEMP/__nodeVersion.tmp"`
       exitWithMessageOnError "getting node version failed"
     fi
-    
+
     if [[ -e "$DEPLOYMENT_TEMP/__npmVersion.tmp" ]]; then
       NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
       exitWithMessageOnError "getting npm version failed"
@@ -106,6 +106,7 @@ selectNodeVersion
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
   cd "$DEPLOYMENT_SOURCE"
   eval $NPM_CMD install
+  eval $NPM_CMD build
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
